9fb558cbb3319672d9e242726d274b7aef35ccfd
viettu2k <doanviettu2k@gmail.com>
2022-08-22 09:30:16 +0700

    finish

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import { Router } from "next/router";
+import "../styles/globals.css";
+import NProgress from "nprogress";
+// import "nprogress/nprogress.css";
+import "../styles/Nprogress.css";
+
+// Route Events
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
