2410fb658e70e7833dc1a779fa67f4732b298743
viettu2k <doanviettu2k@gmail.com>
2022-08-22 09:21:21 +0700

    adding about page

---
+++ pages/about.js
@@ -0,0 +1,34 @@
+//about.js//
+import Link from "next/link";
+
+const about = () => {
+  return (
+    <div>
+      <>
+        <h1>Next.Js Progress Bar Indicator</h1>
+        <ul>
+          <div className="flex">
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+
+            <Link href="/about">
+              <a>About Us</a>
+            </Link>
+
+            <Link href="/blog">
+              <a>Blog Post</a>
+            </Link>
+            <Link href="/contact">
+              <a>Contact Us</a>
+            </Link>
+          </div>
+
+          <h3>About Us</h3>
+        </ul>
+      </>
+    </div>
+  );
+};
+
+export default about;
